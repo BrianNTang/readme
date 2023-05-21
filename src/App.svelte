a110ca676ecc05c9be3c5a46928cfa56887aae2c
Brian Tang <38578221+BrianNTang@users.noreply.github.com>
2023-05-20 17:48:19 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import ProjectCard from "./components/ProjectCard.svelte";
+
+  let projects = [
+    {
+      title: "Project 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      title: "Project 2",
+      description:
+        "Nulla quis tincidunt ex. Vestibulum tincidunt risus vitae feugiat.",
+    },
+  ];
+</script>
+
+<Header title="My Portfolio" />
+
+<main>
+  {#each projects as project}
+    <ProjectCard project="{project}" />
+  {/each}
+</main>
+
+<style>
+  main {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+</style>
