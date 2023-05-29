9516910ee32fb7d44893e52365f50fcca3842817
Brian Tang <38578221+BrianNTang@users.noreply.github.com>
2023-05-29 13:18:38 -0700

    Update use new Svelte page naming conventions

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/readme/", // change to your repo name
+  base: "/readme/",
   plugins: [svelte()],
 })
